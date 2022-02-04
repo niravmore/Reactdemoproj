5f4581b778afbdc724a0775202714cb25e226496
niravmore <68939506+niravmore@users.noreply.github.com>
2022-02-04 17:23:26 +0530

    in development in development

--- src/App.js
+++ src/App.js
@@ -18,17 +18,9 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
+          <p>Login</p>
         </header>
+        <
       </div>
     );
   }
